4bf6def0d969290a20de96a665533f7fad309234
Eric Hopkins <eric.on.tech@gmail.com>
2016-05-26 22:40:01 -0400

    Flesh out Skill class

--- apps/mechwarrior4/skill.js
+++ apps/mechwarrior4/skill.js
@@ -1,7 +1,14 @@
+let _ = require('lodash')
 let xp = require('./xpTable.js')
 
+
 module.exports = class Skill {
-  constructor(name) {
-    this.name = name
+  constructor(data) {
+    this.name = data.name
+    this.subSkills = data.subSkills
+    this.tiered = (data.tiered === undefined ? false : data.tiered)
+    this.targetNumber = data.targetNumber
+    this.complexity = data.complexity
+    this.links = data.links
   }
 }
--- apps/mechwarrior4/tests/skill.js
+++ apps/mechwarrior4/tests/skill.js
@@ -1,9 +1,38 @@
 let expect = require('chai').expect
 let Skill = require('../skill.js')
 
-describe('A skill', function() {
+describe('A Skill', function() {
+  beforeEach(function() {
+    this.skill = new Skill({
+      name: 'Acrobatics',
+      subSkills: ['Free-Fall', 'Gymnastics'],
+      targetNumber: 7,
+      complexity: 'SB',
+      links: ['RFL']
+    })
+  })
+
   it('should have a name', function() {
-    let skill = new Skill('Language')
-    expect(skill.name).to.equal('Language')
+    expect(this.skill.name).to.equal('Acrobatics')
+  })
+
+  it('can have a list of sub-skills', function() {
+    expect(this.skill.subSkills).to.deep.equal(['Free-Fall', 'Gymnastics'])
+  })
+
+  it('should have a tiered attribute', function() {
+    expect(this.skill.tiered).to.equal(false)
+  })
+
+  it('should have a target number', function() {
+    expect(this.skill.targetNumber).to.equal(7)
+  })
+
+  it('should have a complexity rating', function() {
+    expect(this.skill.complexity).to.equal('SB')
+  })
+
+  it('should have linked attributes', function() {
+    expect(this.skill.links).to.deep.equal(['RFL'])
   })
 })
