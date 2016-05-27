fe1cb7f39762cb17b209fbf7a8168dd80c88e8ca
Eric Hopkins <eric.on.tech@gmail.com>
2016-05-26 23:35:45 -0400

    Add handling of tiered skills

--- apps/mechwarrior4/skill.js
+++ apps/mechwarrior4/skill.js
@@ -7,8 +7,34 @@ module.exports = class Skill {
     this.name = data.name
     this.subSkills = data.subSkills
     this.tiered = (data.tiered === undefined ? false : data.tiered)
-    this.targetNumber = data.targetNumber
-    this.complexity = data.complexity
+    this.targetNumbers = data.targetNumbers
+    this.complexities = data.complexities
     this.links = data.links
+    this.xp = data.xp
+  }
+
+  value(learning) {
+    return _(xp[learning]).findLastIndex((x) => _.gte(this.xp, x))
+  }
+
+  targetNumber(learning) {
+    return this.tieredSkill('targetNumbers', learning)
+  }
+
+  complexity(learning) {
+    return this.tieredSkill('complexities', learning)
+  }
+
+  link(learning) {
+    return this.tieredSkill('links', learning)
+  }
+
+  tieredSkill(skill, learning) {
+    if(!this.tiered) {
+      return this[skill][0]
+    } else {
+      if(this.value(learning) <= 3) return this[skill][0]
+      else return this[skill][1]
+    }
   }
 }
--- apps/mechwarrior4/tests/skill.js
+++ apps/mechwarrior4/tests/skill.js
@@ -6,9 +6,10 @@ describe('A Skill', function() {
     this.skill = new Skill({
       name: 'Acrobatics',
       subSkills: ['Free-Fall', 'Gymnastics'],
-      targetNumber: 7,
-      complexity: 'SB',
-      links: ['RFL']
+      targetNumbers: [7],
+      complexities: ['SB'],
+      links: [['RFL']],
+      xp: 40
     })
   })
 
@@ -25,14 +26,51 @@ describe('A Skill', function() {
   })
 
   it('should have a target number', function() {
-    expect(this.skill.targetNumber).to.equal(7)
+    expect(this.skill.targetNumbers).to.deep.equal([7])
   })
 
   it('should have a complexity rating', function() {
-    expect(this.skill.complexity).to.equal('SB')
+    expect(this.skill.complexities).to.deep.equal(['SB'])
   })
 
   it('should have linked attributes', function() {
-    expect(this.skill.links).to.deep.equal(['RFL'])
+    expect(this.skill.links).to.deep.equal([['RFL']])
+  })
+
+  it('should have xp', function() {
+    expect(this.skill.xp).to.equal(40)
+  })
+
+  it('should have a value of 1', function() {
+    expect(this.skill.value('standard')).to.equal(1)
+  })
+
+  describe('When working with a tiered skill', function() {
+    beforeEach(function() {
+      this.skill = new Skill({
+        name: 'Computers',
+        subSkills: [],
+        targetNumbers: [8, 9],
+        tiered: true,
+        complexities: ['CB', 'CA'],
+        links: [['INT'], ['DEX', 'INT']],
+        xp: 80
+      })
+    })
+
+    it('should have the basic tier traits below skill level 3', function() {
+      expect(this.skill.value('standard')).to.equal(3)
+      expect(this.skill.targetNumber('standard')).to.equal(8)
+      expect(this.skill.complexity('standard')).to.equal('CB')
+      expect(this.skill.link('standard')).to.deep.equal(['INT'])
+    })
+
+    it('should have the advanced level traits at or above skill level 4', function() {
+      this.skill.xp += 40
+      expect(this.skill.value('standard')).to.equal(4)
+      expect(this.skill.targetNumber('standard')).to.equal(9)
+      expect(this.skill.complexity('standard')).to.equal('CA')
+      expect(this.skill.link('standard')).to.deep.equal(['DEX', 'INT'])
+    })
   })
 })
