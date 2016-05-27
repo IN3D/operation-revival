1d3ab5e7ecb66528c07eff1fcf4f62e452859634
Eric Hopkins <eric.on.tech@gmail.com>
2016-05-26 21:18:23 -0400

    Ensure that XP Tables have access to lodash

--- apps/mechwarrior4/xpTable.js
+++ apps/mechwarrior4/xpTable.js
@@ -1,4 +1,7 @@
+let _ = require('lodash')
 let standard = [20, 30, 50, 80, 120, 170, 230, 300, 380, 470, 570]
+
+
 module.exports = {
   standard: standard,
   fastLearner: _(standard).map((x) => _.floor(x * 0.9)).value(),
