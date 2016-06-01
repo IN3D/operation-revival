a09a36b78f30d63379ad418804e84935d0536103
Eric Hopkins <eric.on.tech@gmail.com>
2016-06-01 00:46:35 -0400

    Fix errors for JavaScript Standard Style

--- apps/mechwarrior4/tests/character.js
+++ apps/mechwarrior4/tests/character.js
@@ -3,49 +3,49 @@ let Character = require('../character.js')
 let Affiliation = require('../affiliation.js')
 let Skill = require('../skill.js')
 
-describe('A Character', function() {
-  beforeEach(function() {
+describe('A Character', function () {
+  beforeEach(function () {
     this.character = new Character({concept: 'A test character'})
   })
 
-  it('should be able to have a concept during creation', function() {
+  it('should be able to have a concept during creation', function () {
     expect(this.character.concept).to.equal('A test character')
   })
 
-  it('should start with 5,000 XP', function() {
+  it('should start with 5,000 XP', function () {
     expect(this.character.xp).to.equal(5000)
   })
 
-  it('should spend 100 XP to increase an attribute to 1', function() {
+  it('should spend 100 XP to increase an attribute to 1', function () {
     this.character.increaseAttribute('STR', 100)
     expect(this.character.attributes['STR']).to.equal(100)
     expect(this.character.xp).to.equal(4900)
   })
 
-  it('should have an attribute value of 1, if 100 XP has been spent on it', function() {
+  it('should have an attribute value of 1, if 100 XP has been spent on it', function () {
     this.character.increaseAttribute('STR', 100)
     expect(this.character.attribute('STR')).to.equal(1)
   })
 
-  it('should have an attribute value of 3, if 399 XP has been spent on it', function() {
+  it('should have an attribute value of 3, if 399 XP has been spent on it', function () {
     this.character.increaseAttribute('STR', 399)
     expect(this.character.attribute('STR')).to.equal(3)
   })
 
-  describe('When adding skills', function() {
-    beforeEach(function() {
+  describe('When adding skills', function () {
+    beforeEach(function () {
       this.skill = new Skill({name: 'Language', sub: 'English'})
       this.character.increaseSkill(this.skill, 20)
     })
 
-    it('should add a skill, if it does not already have it', function() {
+    it('should add a skill, if it does not already have it', function () {
       expect(this.character.skills).to.deep.equal([
         new Skill({ name: 'Language', sub: 'English', xp: 20 })
       ])
       expect(this.character.xp).to.equal(4980)
     })
 
-    it('should increase a skill, if it already exists', function() {
+    it('should increase a skill, if it already exists', function () {
       this.character.increaseSkill(this.skill, 20)
       expect(this.character.skills).to.deep.equal([
         new Skill({ name: 'Language', sub: 'English', xp: 40 })
@@ -54,8 +54,8 @@ describe('A Character', function() {
     })
   })
 
-  describe('When choosing an affiliation', function() {
-    beforeEach(function() {
+  describe('When choosing an affiliation', function () {
+    beforeEach(function () {
       this.affiliation = new Affiliation({
         cost: 150,
         primaryLanguage: 'English',
@@ -64,16 +64,16 @@ describe('A Character', function() {
       })
     })
 
-    it('should add the affiliation to the characters list of affiliations', function() {
+    it('should add the affiliation to the characters list of affiliations', function () {
       expect(this.character.affiliate(this.affiliation)).to.deep.equal([this.affiliation])
     })
 
-    it('should apply the attribute benefits of the affiliation', function() {
+    it('should apply the attribute benefits of the affiliation', function () {
       this.character.affiliate(this.affiliation)
       expect(this.character.attributes['STR']).to.equal(25)
     })
 
-    it('should apply the negative attributes of the affiliation', function() {
+    it('should apply the negative attributes of the affiliation', function () {
       this.affiliation.attributes[0].value = -50
       this.character.affiliate(this.affiliation)
       expect(this.character.attributes['STR']).to.equal(-50)
--- apps/mechwarrior4/tests/skill.js
+++ apps/mechwarrior4/tests/skill.js
@@ -1,8 +1,8 @@
 let expect = require('chai').expect
 let Skill = require('../skill.js')
 
-describe('A Skill', function() {
-  beforeEach(function() {
+describe('A Skill', function () {
+  beforeEach(function () {
     this.skill = new Skill({
       name: 'Acrobatics',
       subSkills: ['Free-Fall', 'Gymnastics'],
@@ -13,44 +13,44 @@ describe('A Skill', function() {
     })
   })
 
-  it('should have a name', function() {
+  it('should have a name', function () {
     expect(this.skill.name).to.equal('Acrobatics')
   })
 
-  it('should be able to set a sub-skill', function() {
+  it('should be able to set a sub-skill', function () {
     expect(this.skill.subSkill('Free-Fall')).to.equal('Free-Fall')
   })
 
-  it('can have a list of sub-skills', function() {
+  it('can have a list of sub-skills', function () {
     expect(this.skill.subSkills).to.deep.equal(['Free-Fall', 'Gymnastics'])
   })
 
-  it('should have a tiered attribute', function() {
+  it('should have a tiered attribute', function () {
     expect(this.skill.tiered).to.equal(false)
   })
 
-  it('should have a target number', function() {
+  it('should have a target number', function () {
     expect(this.skill.targetNumbers).to.deep.equal([7])
   })
 
-  it('should have a complexity rating', function() {
+  it('should have a complexity rating', function () {
     expect(this.skill.complexities).to.deep.equal(['SB'])
   })
 
-  it('should have linked attributes', function() {
+  it('should have linked attributes', function () {
     expect(this.skill.links).to.deep.equal([['RFL']])
   })
 
-  it('should have xp', function() {
+  it('should have xp', function () {
     expect(this.skill.xp).to.equal(40)
   })
 
-  it('should have a value of 1', function() {
+  it('should have a value of 1', function () {
     expect(this.skill.value('standard')).to.equal(1)
   })
 
-  describe('When working with a tiered skill', function() {
-    beforeEach(function() {
+  describe('When working with a tiered skill', function () {
+    beforeEach(function () {
       this.skill = new Skill({
         name: 'Computers',
         subSkills: [],
@@ -62,14 +62,14 @@ describe('A Skill', function() {
       })
     })
 
-    it('should have the basic tier traits below skill level 3', function() {
+    it('should have the basic tier traits below skill level 3', function () {
       expect(this.skill.value('standard')).to.equal(3)
       expect(this.skill.targetNumber('standard')).to.equal(8)
       expect(this.skill.complexity('standard')).to.equal('CB')
       expect(this.skill.link('standard')).to.deep.equal(['INT'])
     })
 
-    it('should have the advanced level traits at or above skill level 4', function() {
+    it('should have the advanced level traits at or above skill level 4', function () {
       this.skill.xp += 40
       expect(this.skill.value('standard')).to.equal(4)
       expect(this.skill.targetNumber('standard')).to.equal(9)
