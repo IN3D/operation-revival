3dfad77d372f6d5651ba2d95aa855d268c031ac3
Eric Hopkins <eric.on.tech@gmail.com>
2016-06-02 01:41:06 -0400

    Start stylizing a front end

--- entry.js
+++ entry.js
@@ -1,1 +1,6 @@
+require('./style.css')
+require('materialize-css/dist/css/materialize.css')
+require('materialize-css/dist/js/materialize.js')
+require('materialize-css/js/init.js')
+
 document.write('This is working')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,21 @@
+var webpack = require('webpack')
+
 module.exports = {
-  entry: "./entry.js",
+  entry: './entry.js',
   module: {
     loaders: [
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      },
       {
         test: /\.js$/,
         loader: 'babel',
@@ -14,6 +28,12 @@ module.exports = {
   },
   output: {
     path: __dirname,
-    filename: "bundle.js"
-  }
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
 }
